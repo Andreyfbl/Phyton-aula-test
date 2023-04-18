4378fd9566e05d5cbf0390cbe27e41e0d1442511
Andrey Ferreira <andreyferreirahhvj5@gmail.com>
2023-04-17 21:37:32 -0300

    Add files via upload

---
+++ tupla.py
@@ -0,0 +1,15 @@
+num_fav = {
+     'fernanda': 24,
+     'amanda': 7,
+     'andrey': 10,
+     'joao': 23,
+     'alguem do fundao': 14
+}
+
+for nome in num_fav.keys():
+    if nome == 'Alguém do Fundão':
+    print(f'{nome}  ->  {num_fav[nome]}')
+    print(nome)
+    print(num_fav[nome])
+    print('-----------------')
+
